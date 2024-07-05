a9a9e537130cdc7b79961299ee2915385c58e150
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-07-05 20:34:51 -0300

    realizado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import {  fireEvent, render, screen } from '@testing-library/react';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+});
+
+it('Deve adicionar dois comentários', () => {
+    render(<PostComment/>);
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+        target: {
+            value: 'Comentário adicionado por testes',
+        }
+    });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+        target: {
+            value: 'Segundo comentário adicionado por testes',
+        }
+    });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
 });
